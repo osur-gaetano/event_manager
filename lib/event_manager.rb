772dde42fbc5a994a835eb80a8c8f05532e3a34f
Gaetano Osur <165330888+osur-gaetano@users.noreply.github.com>
2024-04-21 19:22:43 +0300

    Add Time and Day of Week targeting

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,6 +6,22 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+def clean_phone_number(phone_number)
+  phone_number = phone_number.to_s.gsub(/[[:punct:] | [:alpha:]]/, '')
+  number_format = "(#{phone_number[0..2]})#{phone_number[3..5]}-#{phone_number[6..]}"
+
+  if (phone_number.length < 10 || phone_number.length > 11) || (phone_number.length == 11 && phone_number[0]!='1')
+    phone_number = 'Bad phone number format'
+  elsif phone_number.size == 11 && phone_number[0] == '1'
+    phone_number = phone_number[1..10]
+    number_format
+    # "(#{phone_number[0..2]})#{phone_number[3..5]}-#{phone_number[6..]}"
+
+  else
+    number_format
+  end
+end
+
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -31,6 +47,13 @@ def save_thank_you_letter(id,form_letter)
   end
 end
 
+def ads_time_targeting(registration_timelines)
+  registration_timelines.reduce(Hash.new(0)) do |peak_timeline, timeline|
+    peak_timeline[timeline] += 1
+    peak_timeline
+  end
+end
+
 puts 'Event Manager Initialized!'
 
 contents = CSV.open(
@@ -41,10 +64,14 @@ contents = CSV.open(
 
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
+peak_registration_hours = []
+peak_registration_days = []
 
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
+  registration_date = row[:regdate]
+  phone_number = clean_phone_number(row[:homephone])
 
   zipcode = clean_zipcode(row[:zipcode])
 
@@ -52,5 +79,16 @@ contents.each do |row|
 
   form_letter = erb_template.result(binding)
 
+  registration_hour = Time.strptime(registration_date, '%m/%d/%y %H:%M').hour
+  peak_registration_hours << registration_hour
+
+  registration_day = Date.strptime(registration_date, '%m/%d/%y %H:%M').wday
+  peak_registration_days << registration_day
+
   save_thank_you_letter(id, form_letter)
 end
+peak_hours = ads_time_targeting(peak_registration_hours)
+puts "Peak hours are #{peak_hours.sort_by { |_, value| -value }.to_h}"
+
+peak_days = ads_time_targeting(peak_registration_days)
+puts "Peak days are #{peak_days.sort_by { |_, value| -value }.to_h}"
